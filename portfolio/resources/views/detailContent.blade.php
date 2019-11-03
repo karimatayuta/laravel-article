cd43eebabbb337140560aeda5951ccf58bf73e21
yuta karimata <ignorantbeginner@gmail.com>
2019-11-04 03:30:43 +0900

    commit

--- portfolio/resources/views/detailContent.blade.php
+++
@@ -1,6 +0,0 @@
-<h1> Hello World </h1>
-@foreach($data as $data)
-<p>{{ $data['user_name'] }}</p>
-@endforeach
-<p></p>
-<p></p>
--- portfolio/routes/web.php
+++ portfolio/routes/web.php
@@ -15,22 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// 新規登録処理
+// 新規投稿登録入力ページ
 Route::resource('/post','PostController');
+// 新規投稿内容確認ページ
 Route::post('/post/create','PostController@create');
+// 新規投稿内容完了ページ
 Route::post('/post/article_regist','PostController@article_regist');
 
-// 記事一覧画面表示
+// 記事一覧ページ表示
 Route::get('/article/list','ArticleController@listView');
-// 記事詳細画面表示
+// 記事詳細ページ表示
 Route::get('/article/detail/{id}','ArticleController@detailView');
-
-
-
-Route::resource('/home_content','HomeContentController');
-
-// 記事詳細ページへの遷移
-Route::get('/detail_content','DetailContentController@index');
-
-
-Route::get('detail_content/{id}', 'DetailContentController@index');
